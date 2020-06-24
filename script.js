192223315f0d4e8e6096db4fdde6320239559a08
Joselucaas <lucas.chag0608@gmail.com>
2020-06-24 11:33:15 -0300

    Site

---
+++ script.js
@@ -0,0 +1,30 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
+
+// for (let card of cards) {
+//     card.addEventListener("click", function() {
+//        modalOverlay.classList.add('active')
+//    })
+// }
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const imageId = card.querySelector("img").getAttribute("id")
+
+        const title = card.children[1].textContent
+        const autor = card.children[2].textContent
+
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("img").src = `img/${imageId}.png`
+
+        modal.querySelector('h1').innerHTML = title
+        modal.querySelector('p').innerHTML = autor
+
+    })
+}
+
+
+document.querySelector('.material-icons').addEventListener('click', function() {
+    modalOverlay.classList.remove('active')
+})
