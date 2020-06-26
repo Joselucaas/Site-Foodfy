19ab30168b40a8d65e15879515a4485f35383140
Joselucaas <lucas.chag0608@gmail.com>
2020-06-25 23:44:39 -0300

    Site Foodfy

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,20 @@ const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 const modal = document.querySelector('.modal')
 
-for(let card of cards){
-    card.addEventListener("click", function(){
-        const imageId = card.querySelector('img').getAttribute("id")
+for(let card of cards) {
+    card.addEventListener('click', function() {
 
+        const idImage = card.querySelector('img').getAttribute('id')
         const title = card.children[1].textContent
         const autor = card.children[2].textContent
 
-        modalOverlay.classList.add("active")
-        modalOverlay.querySelector("img").src = `img/${imageId}.png`
-
-        modal.querySelector('h1').innerHTML = title
-        modal.querySelector('p').innerHTML = autor
-
+        modalOverlay.classList.add('active')
+        
+        modal.querySelector('img').src = `/img/${idImage}.png`
+        modal.querySelector('p').innerHTML = title
+        modal.querySelector('h2').innerHTML = autor
     })
 }
-
 
 document.querySelector('.material-icons').addEventListener('click', function() {
     modalOverlay.classList.remove('active')
